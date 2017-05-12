433c6f590e7ecbcf46e24c7d06f77750629586fc
Wsuwka <kat.krzeminska.kat@gmail.com>
2017-05-12 08:01:42 +0000

    Piny

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508105123) do
+ActiveRecord::Schema.define(version: 20170512075058) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
